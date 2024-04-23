43b8be6d2c2d70b0a86964aadbd7f9f4c8f6b816
Jonas Christiansen <j.christiansen@tu-berlin.de>
2024-04-23 18:19:09 +0200

    sync // dataclasses, yet nothing noticeable

--- eflips/ingest/vdv2.py
+++ eflips/ingest/vdv2.py
@@ -4,6 +4,7 @@ import glob
 import logging
 import os
 from dataclasses import dataclass
+from vdv452data import vdv452_v1_5
 
 
 class VDV_Table_Name(enum.Enum):
@@ -37,144 +38,7 @@ class VDV_Table_Name(enum.Enum):
     REC_UMLAUF = "REC_UMLAUF"
 
 
-@dataclass
-class EingangsdatenTabelle:
-    abs_file_path: str
-    character_set: str
-    table_name: VDV_Table_Name
-
-
-def check_vdv451_file_header(abs_file_path: str) -> EingangsdatenTabelle:
-    """
-    Checks the contents of a VDV 451 (.x10) file, TODO
-    :param file_path: The ABSOLUTE path to the VDV 451 file
-    :return: TODO
-
-    """
-
-    # 1. Open file and recognize the encoding
-    # For VDV 451, either ASCII or ISO8859-1 is allowed as encoding for the table datasets. However, the header is always ASCII (see Ch. 4.1 of VDV 451).
-    # Therefore, we open the file with ISO8859-1
-    # (and return an error if it is not ASCII or ISO8859-1).
-    logger = logging.getLogger(__name__)
-
-    table_name_str = None
-    character_set = None
-
-    valid_character_sets = ["ASCII", "ISO8859-1", "ISO-8859-1"]
-
-    try:
-        with open(abs_file_path, "r", encoding="ISO8859-1") as f:
-            for line in f:
-                if line.strip().split(";")[0] == "chs":
-                    # For these modes, we need to utilize the CSV reader here in order to get rid of the double quote marks enclosing the strings (otherwise, we would have e.g. '"Templin, ZOB"') etc.
-                    parts_csvrdr = csv.reader([line], delimiter=";", skipinitialspace=True)
-                    parts = list(parts_csvrdr)[0]
-
-                else:
-                    # The other modes should be uncritical as they do not contain those double quotes or we dont need the info in them
-                    parts = line.strip().split(";")
-
-                # Handling of the line based on the specific "command" (see VDV 451 documentation)
-                command = parts[0]
-
-                if command == "tbl":
-                    # Get the table name (e.g. 'MENGE_BASIS_VERSIONEN')
-                    table_name_str = parts[1].upper().strip()
-
-                elif command == "chs":
-                    # Get the character set used in the file
-                    character_set = parts[1].upper().strip()
-                    if character_set not in valid_character_sets:
-                        raise ValueError(
-                            "The file",
-                            abs_file_path,
-                            " uses an encoding that is not allowed according to the VDV 451 specification:"
-                            + character_set
-                            + " does not match 'ASCII' or 'ISO8859-1'.",
-                        )
-
-                elif command == "rec":
-                    if table_name_str is not None and character_set is not None:
-                        # We have all necessary information (and it contains at least one record)
-                        break
-
-                elif command == "eof":
-                    # We reached the end of the file without seeing any records
-                    raise ValueError("The file" + str(abs_file_path) + " does not contain any records.")
-
-    except UnicodeDecodeError as e:
-        e.add_note(
-            "The header of the file"
-            + str(abs_file_path)
-            + " is using an encoding that contains non-ASCII characters. This is not allowed according to the VDV 451 specification.",
-        )
-        raise e
-
-    # Raise an error if table name or encoding is not found
-    if table_name_str is None:
-        msg = f"The file {abs_file_path} does not contain a table name in the header."
-        logger.info(msg)
-        raise ValueError(msg)
-    if character_set is None:
-        msg = f"The file {abs_file_path} does not contain a character set in the header."
-        logger.info(msg)
-        raise ValueError(msg)
-
-    if table_name_str not in [x.value for x in VDV_Table_Name]:
-        raise ValueError(
-            "The file" + str(abs_file_path) + " contains an unknown table name: " + table_name_str + " Skipping it."
-        )
-
-    return EingangsdatenTabelle(
-        abs_file_path=abs_file_path, character_set=character_set, table_name=VDV_Table_Name[table_name_str]
-    )
-
-
-def validate_input_data_vdv_451(abs_path_to_folder_with_vdv_files: str) -> dict[VDV_Table_Name, EingangsdatenTabelle]:
-    """
-    Checks if the given directory contains all necessary .x10 files (necessary as in VDV 451/452 specified)
-    and xxx.
-    :param abs_path_to_folder_with_vdv_files: The ABSOLUTE path to the directory containing the VDV 451 files
-    :return: TBC TODO
-    """
-    logger = logging.getLogger(__name__)
-
-    # Create a Pattern to find all .x10 Files in this directory
-    # in macOS (unlike windows) searching for *.x10 wiles will NOT find files with the extension .X10.
-    # Therefore, we need to search for both, but also filter out the duplicates later as we would otherwise have duplicates in windows
-    search_pattern_lowercase = os.path.join(abs_path_to_folder_with_vdv_files, "*.x10")
-    search_pattern_uppercase = os.path.join(abs_path_to_folder_with_vdv_files, "*.X10")
-
-    # Find all files that match this pattern.
-    x10_files = glob.glob(search_pattern_lowercase) + glob.glob(search_pattern_uppercase)
-    x10_files_unique = list(set(x10_files))
-
-    # Iterate through the files, checking whether the neccessary tables are present
-
-    # VDV 451 has two naming schemes. One is the name of the table directly as the name, the other is some number combination,
-    # see VDV 451 Chapter 3.1 and 3.2. However, as the name of the table is also included in the file contents, we instead
-    # check the contents of each file to determine to which table it belongs.
-
-    all_tables: dict[VDV_Table_Name, EingangsdatenTabelle] = {}
-    for abs_file_path in x10_files_unique:
-        try:
-            eingangsdatentable: EingangsdatenTabelle = check_vdv451_file_header(abs_file_path)
-
-            # Check if the table name is already present in the dictionary (would mean duplicate, two times the same table in the files)
-            if eingangsdatentable.table_name in all_tables.keys():
-                raise ValueError(
-                    "The table " + eingangsdatentable.table_name.value + " is present in multiple files. Aborting."
-                )
-
-            else:
-                all_tables[eingangsdatentable.table_name] = eingangsdatentable
-
-        except (ValueError, UnicodeDecodeError) as e:
-            msg = "While processing " + abs_file_path + " the following exception occurred: "
-            logger.warning(msg, exc_info=e)
-            continue
-
+class VDV_Util:
     # Required Tables:
     # BASIS_VER_GUELTIGKEIT: Nötig, um herauszufinden welches die aktuell gültige Version ist (kein Handling, falls nicht existent und trz nur 1 Version existiert)
     # FIRMENKALENDER: Brauchen wir wegen zuordnung Betriebstage zu Tagesarten (für Umläufe)
@@ -223,22 +87,203 @@ def validate_input_data_vdv_451(abs_path_to_folder_with_vdv_files: str) -> dict[
     # MENGE_FAHRTART: Textuelle Beschreibung der Fahrtarten (z.B. Normalfahrt, Betriebshofausfahrt usw. siehe VDV 452 Kap. 9.6.8)
 
     # fahrzeug waere optional, aber machen wir nicht?
-    required_tables = [
-        VDV_Table_Name.BASIS_VER_GUELTIGKEIT,
-        VDV_Table_Name.FIRMENKALENDER,
-        VDV_Table_Name.REC_ORT,
-        VDV_Table_Name.MENGE_FZG_TYP,
-        VDV_Table_Name.REC_SEL,
-        VDV_Table_Name.SEL_FZT_FELD,
-        VDV_Table_Name.LID_VERLAUF,
-        VDV_Table_Name.REC_FRT,
-        VDV_Table_Name.REC_UMLAUF,
-        VDV_Table_Name.REC_LID,  # hmm
-    ]
 
-    if not set(required_tables) <= set(all_tables.keys()):
+    # required tables: die Enum Datatypes als keys und das Dataclass object als value.
+    required_tables = {
+        VDV_Table_Name.BASIS_VER_GUELTIGKEIT: vdv452_v1_5.BasisVerGueltigkeit,
+        VDV_Table_Name.FIRMENKALENDER: vdv452_v1_5.Firmenkalender,
+        VDV_Table_Name.REC_ORT: vdv452_v1_5.RecOrt,
+        VDV_Table_Name.MENGE_FZG_TYP: vdv452_v1_5.MengeFzgTyp,
+        VDV_Table_Name.REC_SEL: vdv452_v1_5.RecSel,
+        VDV_Table_Name.SEL_FZT_FELD: vdv452_v1_5.SelFztFeld,
+        VDV_Table_Name.LID_VERLAUF: vdv452_v1_5.LidVerlauf,
+        VDV_Table_Name.REC_FRT: vdv452_v1_5.RecFrt,
+        VDV_Table_Name.REC_UMLAUF: vdv452_v1_5.RecUmlauf,
+        VDV_Table_Name.REC_LID: vdv452_v1_5.RecLid,  # hmm
+    }
+
+
+@dataclass
+class EingangsdatenTabelle:
+    abs_file_path: str
+    character_set: str
+    table_name: VDV_Table_Name
+
+
+def check_vdv451_file_header(abs_file_path: str) -> EingangsdatenTabelle:
+    """
+    Checks the contents of a VDV 451 (.x10) file, TODO
+    :param file_path: The ABSOLUTE path to the VDV 451 file
+    :return: TODO
+
+    """
+
+    # 1. Open file and recognize the encoding
+    # For VDV 451, either ASCII or ISO8859-1 is allowed as encoding for the table datasets. However, the header is always ASCII (see Ch. 4.1 of VDV 451).
+    # Therefore, we open the file with ISO8859-1
+    # (and return an error if it is not ASCII or ISO8859-1).
+    logger = logging.getLogger(__name__)
+
+    table_name_str = None
+    character_set = None
+
+    valid_character_sets = ["ASCII", "ISO8859-1"]
+
+    try:
+        with open(abs_file_path, "r", encoding="ISO8859-1") as f:
+            for line in f:
+                if line.strip().split(";")[0] == "chs":
+                    # For these modes, we need to utilize the CSV reader here in order to get rid of the double quote marks enclosing the strings (otherwise, we would have e.g. '"Templin, ZOB"') etc.
+                    parts_csvrdr = csv.reader([line], delimiter=";", skipinitialspace=True)
+                    parts = list(parts_csvrdr)[0]
+
+                else:
+                    # The other modes should be uncritical as they do not contain those double quotes or we dont need the info in them
+                    parts = line.strip().split(";")
+
+                # Handling of the line based on the specific "command" (see VDV 451 documentation)
+                command = parts[0]
+
+                if command == "tbl":
+                    # Get the table name (e.g. 'MENGE_BASIS_VERSIONEN')
+                    table_name_str = parts[1].upper().strip()
+
+                elif command == "chs":
+                    # Get the character set used in the file
+                    character_set = parts[1].upper().strip()
+
+                    # Oftentimes, the Character set is accidentally named as ISO-8859-1 (additional dash).
+                    # Fix the character set if it is ISO-8859-1 to the correct form
+                    if character_set == "ISO-8859-1":
+                        character_set = "ISO8859-1"
+
+                    if character_set not in valid_character_sets:
+                        raise ValueError(
+                            "The file",
+                            abs_file_path,
+                            " uses an encoding that is not allowed according to the VDV 451 specification:"
+                            + character_set
+                            + " does not match 'ASCII' or 'ISO8859-1'.",
+                        )
+
+                elif command == "rec":
+                    if table_name_str is not None and character_set is not None:
+                        # We have all necessary information (and it contains at least one record)
+                        break
+
+                elif command == "eof":
+                    # We reached the end of the file without seeing any records
+                    raise ValueError("The file" + str(abs_file_path) + " does not contain any records.")
+
+    except UnicodeDecodeError as e:
+        e.add_note(
+            "The header of the file"
+            + str(abs_file_path)
+            + " is using an encoding that contains non-ASCII characters. This is not allowed according to the VDV 451 specification.",
+        )
+        raise e
+
+    # Raise an error if table name or encoding is not found
+    if table_name_str is None:
+        msg = f"The file {abs_file_path} does not contain a table name in the header."
+        logger.info(msg)
+        raise ValueError(msg)
+    if character_set is None:
+        msg = f"The file {abs_file_path} does not contain a character set in the header."
+        logger.info(msg)
+        raise ValueError(msg)
+
+    if table_name_str not in [x.value for x in VDV_Table_Name]:
+        raise ValueError(
+            "The file" + str(abs_file_path) + " contains an unknown table name: " + table_name_str + " Skipping it."
+        )
+
+    return EingangsdatenTabelle(
+        abs_file_path=abs_file_path, character_set=character_set, table_name=VDV_Table_Name[table_name_str]
+    )
+
+
+def import_vdv452_table_records(EingangsdatenTabelle: EingangsdatenTabelle) -> None:
+    """
+    Imports the records of a VDV 451 table into the database.
+    :param EingangsdatenTabelle: The EingangsdatenTabelle object containing the table name and the path to the file
+    :return: None
+    """
+    logger = logging.getLogger(__name__)
+
+    # Open the file
+    try:
+        with open(EingangsdatenTabelle.abs_file_path, "r", encoding=EingangsdatenTabelle.character_set) as f:
+            for line in f:
+                if line.strip().split(";")[0] == "rec":
+                    # For this mode, we need to utilize the CSV reader here in order to get rid of the double quote marks enclosing the strings (otherwise, we would have e.g. '"Templin, ZOB"') etc.
+                    parts_csvrdr = csv.reader([line], delimiter=";", skipinitialspace=True)
+                    parts = list(parts_csvrdr)[0]
+
+                    row_data = parts[1:]
+
+    except UnicodeDecodeError as e:
+        # todo specify more in detail where exactly the unicode error occurred?
+        e.add_note(
+            "The file"
+            + str(EingangsdatenTabelle.abs_file_path)
+            + " is using an encoding that contains non-"
+            + EingangsdatenTabelle.character_set
+            + " characters, thus not matching the specified encoding. Aborting.",
+        )
+        raise e
+
+
+def validate_input_data_vdv_451(abs_path_to_folder_with_vdv_files: str) -> dict[VDV_Table_Name, EingangsdatenTabelle]:
+    """
+    Checks if the given directory contains all necessary .x10 files (necessary as in VDV 451/452 specified)
+    and xxx.
+    :param abs_path_to_folder_with_vdv_files: The ABSOLUTE path to the directory containing the VDV 451 files
+    :return: TBC TODO
+    """
+    logger = logging.getLogger(__name__)
+
+    # Create a Pattern to find all .x10 Files in this directory
+    # in macOS (unlike windows) searching for *.x10 wiles will NOT find files with the extension .X10.
+    # Therefore, we need to search for both, but also filter out the duplicates later as we would otherwise have duplicates in windows
+    search_pattern_lowercase = os.path.join(abs_path_to_folder_with_vdv_files, "*.x10")
+    search_pattern_uppercase = os.path.join(abs_path_to_folder_with_vdv_files, "*.X10")
+
+    # Find all files that match this pattern.
+    x10_files = glob.glob(search_pattern_lowercase) + glob.glob(search_pattern_uppercase)
+    x10_files_unique = list(set(x10_files))
+
+    # Iterate through the files, checking whether the neccessary tables are present
+
+    # VDV 451 has two naming schemes. One is the name of the table directly as the name, the other is some number combination,
+    # see VDV 451 Chapter 3.1 and 3.2. However, as the name of the table is also included in the file contents, we instead
+    # check the contents of each file to determine to which table it belongs.
+
+    all_tables: dict[VDV_Table_Name, EingangsdatenTabelle] = {}
+    for abs_file_path in x10_files_unique:
+        try:
+            eingangsdatentable: EingangsdatenTabelle = check_vdv451_file_header(abs_file_path)
+
+            # Check if the table name is already present in the dictionary (would mean duplicate, two times the same table in the files)
+            if eingangsdatentable.table_name in all_tables.keys():
+                raise ValueError(
+                    "The table " + eingangsdatentable.table_name.value + " is present in multiple files. Aborting."
+                )
+
+            else:
+                all_tables[eingangsdatentable.table_name] = eingangsdatentable
+
+        except (ValueError, UnicodeDecodeError) as e:
+            msg = "While processing " + abs_file_path + " the following exception occurred: "
+            logger.warning(msg, exc_info=e)
+            continue
+
+    # Required Tables:
+    # siehe ganz oben in der File fuer explanation
+
+    if not set(VDV_Util.required_tables.keys()) <= set(all_tables.keys()):
         # Compute all tables that are required but not in the tables in the files, to display them to the user
-        missing_tables = set(required_tables) - set(all_tables.keys())
+        missing_tables = set(VDV_Util.required_tables.keys()) - set(all_tables.keys())
         missing_tables_str = " ".join([x.value + ", " for x in missing_tables])
         raise ValueError(
             "Not all necessary tables are present in the directory (or present, but empty). Missing tables are: "
